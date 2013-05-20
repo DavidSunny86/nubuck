290bdd53783c287f3bc6d2c6caf9cc11b909aa81
Christian Jaeger <johndoe.foobar@googlemail.com>
2013-05-20 14:17:29 +0200

    tweaked GL_CALL

--- src/Nubuck/renderer/glcall.h
+++ src/Nubuck/renderer/glcall.h
@@ -2,22 +2,26 @@
 
 #include <common\common.h>
 
+#ifdef NUBUCK_GLCALL
 #define GL_CALL(x) \
     do { \
         x; \
         GLint error; \
         while((error = glGetError())) { \
-            common.printf("calling %s failed with error %X in %s:%d\n", \
-                #x, error, __FILE__, __LINE__); \
+            common.printf("ERROR - in %s:%d: calling %s failed with error 0x%X, '%s'.\n", \
+                __FILE__, __LINE__, #x, error, gluErrorString(error)); \
             Crash(); \
         } \
     } while(false)
+#else
+#define GL_CALL(x) x
+#endif // NUBUCK_GLCALL
 
 #define GL_CHECK_ERROR \
     do { \
         GLint error; \
         if((error = glGetError())) { \
-            common.printf("ERROR - in %s:%d: glGetError() == %d, '%s'.\n", \
+            common.printf("ERROR - in %s:%d: glGetError() == 0x%X, '%s'.\n", \
                 __FILE__, __LINE__, error, gluErrorString(error)); \
             Crash(); \
         } \
