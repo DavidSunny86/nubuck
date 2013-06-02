6b7a279b00bdd53bbe9f50b888ea34aa858adb13
Christian Jaeger <johndoe.foobar@googlemail.com>
2013-06-02 11:07:29 +0200

    added texture loading

--- src/Nubuck/common/common.cpp
+++ src/Nubuck/common/common.cpp
@@ -6,6 +6,64 @@
 
 #include "common.h"
 
+int COM_Tokenize(ctoken_t** tokens, const char* string) {
+	ctoken_t* token;
+	ctoken_t* head = NULL;
+	ctoken_t* tail = NULL;
+	bool reading = false;
+	const char* strPtr = string;
+	char* tokenPtr;
+	int num = 0;
+	int i;
+
+	COM_assert(NULL != tokens);
+
+	while('\0' != *strPtr) {
+		if(' ' == *strPtr || '\t' == *strPtr) {
+			if(reading) reading = false;
+			strPtr++;
+			continue;
+		}
+		if(!reading) {
+			token = (ctoken_t*)malloc(sizeof(ctoken_t));
+			if(!token) {
+                common.printf("out of memory tokenizing string.\n");
+				return -1;
+			}
+			// for(i = 0; i < MAX_TOKEN; ++i) token->string[i] = '\0';
+			memset(token->string, 0, MAX_TOKEN);
+			token->next = NULL;
+			if(tail) tail->next = token;
+			else head = token;
+			tail = token;
+			
+			tokenPtr = token->string;
+			reading = true;
+		}
+		*tokenPtr++ = *strPtr++;
+	}
+
+	token = head;
+	while(NULL != token) {
+		num++;
+		sscanf(token->string, "%11f", &token->f);
+		sscanf(token->string, "%d", &token->i);
+		token = token->next;
+	}
+
+	*tokens = head;
+	return num;
+}
+
+void COM_FreeTokens(ctoken_t* tokens) {
+	ctoken_t* tmp;
+	while(NULL != tokens) {
+		tmp = tokens->next;
+		free(tokens);
+		tokens = tmp;
+	}
+}
+
 static std::string DirOf(const std::string& path) {
     const char* delim = "/\\";
     return path.substr(0, path.find_last_of(delim));
---
+++ src/Nubuck/renderer/textures/texture.cpp
@@ -0,0 +1,98 @@
+#include <common\common.h>
+#include "..\glcall.h"
+#include "texture.h"
+
+namespace R {
+
+	/* Texture Impl */
+
+	void Texture::Init(const COM::byte_t* pixelData) {
+		COM_assert(0 < _width);
+		COM_assert(0 < _height);
+
+		GL_CALL(glGenTextures(1, &_id));
+		GL_CALL(glBindTexture(GL_TEXTURE_2D, _id));
+		
+		GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, _internalFormat, _width, _height, 0, _format,
+			GL_UNSIGNED_BYTE, pixelData));
+
+		GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR));
+		GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR));
+		/*
+		GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+		GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+		*/
+		float borderColor[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+		GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER));
+		GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER));
+		GL_CALL(glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor));
+	}
+
+	Texture::Texture(int width, int height, GLenum internalFormat, GLenum format,
+		const COM::byte_t* pixelData)
+		: _internalFormat(internalFormat), _format(format), _width(width), _height(height)
+	{
+		Init(pixelData);
+	}
+
+	Texture::Texture(int width, int height, GLenum format, const COM::byte_t* pixelData) 
+		: _internalFormat(format), _format(format), _width(width), _height(height)
+	{
+		Init(pixelData);
+	}
+
+	void Texture::Bind(unsigned level) const {
+		GL_CALL(glActiveTexture(GL_TEXTURE0 + level));
+		GL_CALL(glBindTexture(GL_TEXTURE_2D, _id));
+	}
+
+	/* TextureManager Impl */
+
+	std::string ToLower(const std::string& str) {
+		std::string ret;
+		for(std::size_t i = 0; i < str.length(); ++i) {
+			char c = str[i];
+			if(c >= 'A' && c <= 'Z') ret.push_back('a' + (c - 'A'));
+			else ret.push_back(c);
+		}
+		return ret;
+	}
+
+	std::string Suffix(const std::string& str) {
+		std::string suffix;
+		std::size_t pos = str.find_last_of('.');
+		if(std::string::npos != pos) {
+			suffix = str.substr(pos + 1);
+		}
+		return suffix;
+	}
+
+	void TextureManager::RegisterAlloc(const std::string& suffix, texAlloc_t alloc) {
+		if(std::string::npos != suffix.find('.')) {
+            common.printf("TextureManager::RegisterAlloc: suffix %s contains '.'\n", suffix.c_str());
+		}
+		_allocs[ToLower(suffix)] = alloc;
+	}
+
+	GEN::Pointer<Texture> TextureManager::Get(const std::string& filename, FS::Loader& loader) {
+		texIt_t texIt = _textures.find(filename);
+		if(_textures.end() == texIt) {
+			std::string suffix = ToLower(Suffix(filename));
+			allocIt_t allocIt = _allocs.find(suffix);
+			if(_allocs.end() == allocIt) {
+                common.printf("TextureManager::Get: for suffix '%s' of filename '%s' is no allocator registered.\n",
+                    suffix.c_str(), filename.c_str());
+				throw std::exception();
+			}
+
+			GEN::Pointer<FS::File> file = loader.GetFile(filename);
+			GEN::Pointer<Texture> texture(allocIt->second(file));
+			_textures[filename] = texture;
+
+			return texture;
+		} else {
+			return texIt->second;
+		}
+	}
+
+} // namespace R
