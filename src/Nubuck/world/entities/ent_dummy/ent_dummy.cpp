07053af1272758d6e81c59a667d8cf7b04d95789
Christian Jaeger <johndoe.foobar@googlemail.com>
2013-07-03 17:53:26 +0200

    draw beautiful bezier triangle

--- src/Nubuck/world/entities/ent_dummy/ent_dummy.cpp
+++ src/Nubuck/world/entities/ent_dummy/ent_dummy.cpp
@@ -3,10 +3,24 @@
 namespace W {
 
     ENT_Dummy::ENT_Dummy(void) {
+        const M::Vector2 c0(-1.0f, -1.0f);
+        const M::Vector2 c1( 1.0f, -1.0f);
+        const M::Vector2 c2( 0.0f,  1.0f);
+        const float lf = 0.2f, ln = (1.0f - lf);
         std::vector<M::Vector2> points;
-        points.push_back(M::Vector2(-1.0f, 0.0f));
-        points.push_back(M::Vector2( 1.0f, 0.0f));
-        points.push_back(M::Vector2( 0.0f, 1.0f));
+        points.push_back(ln * c0 + lf * c1);
+        points.push_back(0.5f * c0 + 0.5f * c1);
+        points.push_back(lf * c0 + ln * c1);
+        points.push_back(c1);
+        points.push_back(ln * c1 + lf * c2);
+        points.push_back(0.5f * c1 + 0.5f * c2);
+        points.push_back(lf * c1 + ln * c2);
+        points.push_back(c2);
+        points.push_back(ln * c2 + lf * c0);
+        points.push_back(0.5f * c2 + 0.5f * c0);
+        points.push_back(lf * c2 + ln * c0);
+        points.push_back(c0);
+        points.push_back(ln * c0 + lf * c1);
         _bezierCurve = GEN::Pointer<R::PolyBezier2U>(new R::PolyBezier2U(points));
         _mesh = R::meshMgr.Create(_bezierCurve->GetDesc());
         _material.diffuseColor = R::Color::Black;
