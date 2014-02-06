dcbdd3f509f870cf5f33dce0070c2bf5772896a8
Christian Jaeger <johndoe.foobar@googlemail.com>
2014-02-06 10:58:33 +0100

    added LoadOBJ operator

--- src/Nubuck/operators/op_loadobj/op_loadobj.cpp
+++ src/Nubuck/operators/op_loadobj/op_loadobj.cpp
@@ -1,9 +1,42 @@
+#include <QFileDialog>
 #include <QMenu>
+#include <QLabel>
+#include <QHBoxLayout>
 
+#include <world\entities\ent_geometry\ent_geometry.h>
 #include <Nubuck\polymesh.h>
 #include "op_loadobj.h"
 
 namespace OP {
+
+void LoadOBJ::OnChooseFilename() {
+	QString filename = QFileDialog::getOpenFileName(
+        _panel,
+        "Choose a .obj file",
+		QDir::currentPath(),
+        tr("Models (*.obj)"));
+	if(!filename.isNull()) {
+		_ui.lneFilename->setText(filename);
+
+        if(_geom) _geom->Destroy();
+
+        _geom = (W::ENT_Geometry*)_nb.world->CreateGeometry();
+        _geom->SetRenderMode(IGeometry::RenderMode::NODES | IGeometry::RenderMode::EDGES | IGeometry::RenderMode::FACES);
+        leda::nb::RatPolyMesh& mesh = _geom->GetRatPolyMesh();
+		mesh.FromObj(filename.toAscii());
+        _geom->Update();
+
+        _nb.world->SelectGeometry(_geom);
+	}
+}
+
+void LoadOBJ::BuildPanel() {
+    _panel = new QWidget();
+	_ui.setupUi(_panel);
+	_nb.ui->SetOperatorPanel(_panel);
+
+	QObject::connect(_ui.btnChooseFile, SIGNAL(clicked()), this, SLOT(OnChooseFilename()));
+}
 
 void LoadOBJ::Register(const Nubuck& nb, Invoker& invoker) {
     _nb = nb;
@@ -15,13 +48,8 @@ void LoadOBJ::Register(const Nubuck& nb, Invoker& invoker) {
 void LoadOBJ::Invoke() {
     printf("LoadOBJ::Invoke\n");
 
-    IGeometry* geom = _nb.world->CreateGeometry();
-    geom->SetRenderMode(IGeometry::RenderMode::NODES | IGeometry::RenderMode::EDGES | IGeometry::RenderMode::FACES);
-    leda::nb::RatPolyMesh& mesh = geom->GetRatPolyMesh();
-    mesh.FromObj("bunny2.obj");
-    geom->Update();
-
-    _nb.world->SelectGeometry(geom);
+    _geom = NULL;
+    BuildPanel();
 }
 
 } // namespace OP
