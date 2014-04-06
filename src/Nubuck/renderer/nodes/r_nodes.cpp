bd76acf186f560f238134f21c78da619b9804cbe
Christian Jaeger <johndoe.foobar@googlemail.com>
2014-04-06 16:28:03 +0200

    removed dead code
    removed dead functions Billboard_FaceCamera(), Billboard_FaceViewingPlane()

--- src/Nubuck/renderer/nodes/r_nodes.cpp
+++ src/Nubuck/renderer/nodes/r_nodes.cpp
@@ -19,30 +19,6 @@ Nodes::~Nodes() {
     // DestroyMesh();
 }
 
-// camera position at 0
-static M::Matrix4 Billboard_FaceCamera(const M::Matrix4& worldMat, const M::Vector3& billboardPos) {
-    const M::Vector3 p = M::Transform(worldMat, billboardPos);
-    const M::Vector3 v2 = M::Normalize(-M::Vector3(p.x, 0.0f, p.z));
-    const M::Vector3 v1 = M::Vector3(0.0f, 1.0f, 0.0f);
-    const M::Vector3 v0 = M::Normalize(M::Cross(v1, v2));
-    const M::Matrix3 M = M::Mat3::FromColumns(v0, v1, v2);
-    const M::Vector3 w2 = M::Normalize(-M::Vector3(0.0f, p.y, p.z));
-    const M::Vector3 w0 = M::Vector3(1.0f, 0.0f, 0.0f);
-    const M::Vector3 w1 = M::Normalize(M::Cross(w0, w2));
-    const M::Matrix3 N = M::Mat3::FromColumns(w0, w1, w2);
-    return M::Mat4::FromRigidTransform(M * N, p);
-}
-
-// omits scale
-static M::Matrix4 Billboard_FaceViewingPlane(const M::Matrix4& worldMat, const M::Vector3& billboardPos) {
-    const M::Vector3 p = M::Transform(worldMat, billboardPos);
-    return M::Matrix4(
-        1.0f, 0.0f, 0.0f, worldMat.m03 + p.x,
-        0.0f, 1.0f, 0.0f, worldMat.m13 + p.y,
-        0.0f, 0.0f, 1.0f, worldMat.m23 + p.z,
-        0.0f, 0.0f, 0.0f, 1.0f);
-}
-
 void Nodes::Rebuild(const std::vector<Node>& nodes) {
 	SYS::ScopedLock lock(_mtx);
 
