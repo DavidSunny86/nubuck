cabf1e7968f90bb341c5483a5e92388ceb702580
Christian Jaeger <johndoe.foobar@googlemail.com>
2014-04-30 12:35:14 +0200

    op_delaunay3d: added explosion scale

--- src/Nubuck/operators/op_delaunay3d/op_delaunay3d.cpp
+++ src/Nubuck/operators/op_delaunay3d/op_delaunay3d.cpp
@@ -5,10 +5,49 @@
 #include <LEDA\geo\d3_delaunay.h>
 
 #include <Nubuck\polymesh.h>
+#include <UI\block_signals.h>
 #include "d3_delaunay.h"
 #include "op_delaunay3d.h"
 
 namespace OP {
+
+void Delaunay3DPanel::OnScaleChanged(int value) {
+    EV::Params_OP_Delaunay3D_SetScale args;
+    args.value = value;
+    args.max = _ui.sldScale->maximum();
+    SendToOperator(EV::def_OP_Delaunay3D_SetScale.Create(args));
+}
+
+Delaunay3DPanel::Delaunay3DPanel(QWidget* parent) : OperatorPanel(parent) {
+    _ui.setupUi(this);
+}
+
+void Delaunay3DPanel::Invoke() {
+    UI::BlockSignals block(_ui.sldScale);
+    _ui.sldScale->setValue(0);
+}
+
+void Delaunay3D::Event_SetScale(const EV::Event& event) {
+    const EV::Params_OP_Delaunay3D_SetScale& args = EV::def_OP_Delaunay3D_SetScale.GetArgs(event);
+
+    if(_simplices.empty()) return;
+
+    for(unsigned i = 0; i < _simplices.size(); ++i) {
+        Simplex& simplex = _simplices[i];
+
+        leda::rational scale = 1 + 5 * leda::rational(args.value, args.max);
+        leda::rat_vector center = scale * simplex.center;
+
+        simplex.geom->SetPosition(
+            center.xcoord().to_float(),
+            center.ycoord().to_float(),
+            center.zcoord().to_float());
+    }
+}
+
+Delaunay3D::Delaunay3D() {
+    AddEventHandler(EV::def_OP_Delaunay3D_SetScale, this, &Delaunay3D::Event_SetScale);
+}
 
 void Delaunay3D::Register(const Nubuck& nb, Invoker& invoker) {
     _nb = nb;
@@ -22,6 +61,8 @@ void Delaunay3D::Invoke() {
 
     _nb.ui->SetOperatorName("Delaunay 3D");
 
+    _simplices.clear();
+
     std::vector<IGeometry*> geomSel = _nb.world->GetSelection()->GetList();
     if(geomSel.empty()) return;
 
@@ -32,11 +73,11 @@ void Delaunay3D::Invoke() {
     leda::node v;
     forall_nodes(v, cloudMesh) L.push_back(cloudMesh.position_of(v));
 
-    leda::list<leda::fork::simplex_t> simplices;
+    leda::list<leda::fork::simplex_t> S;
 
     std::cout << "Delaunay3D: calling D3_DELAUNAY ... " << std::flush;
     // NOTE: include d3_delaunay.cpp when building LEDA
-    leda::fork::D3_DELAUNAY(L, simplices);
+    leda::fork::D3_DELAUNAY(L, S);
     std::cout << "DONE" << std::endl;
 
     float x, y, z;
@@ -44,15 +85,15 @@ void Delaunay3D::Invoke() {
 
     std::cout << "Delaunay3D: creating simplex geometries ... " << std::flush;
     leda::list_item it;
-    forall_items(it, simplices) {
-        IGeometry* simplex = _nb.world->CreateGeometry();
-        simplex->SetRenderMode(IGeometry::RenderMode::NODES | IGeometry::RenderMode::EDGES | IGeometry::RenderMode::FACES);
-        leda::nb::RatPolyMesh& simplexMesh = simplex->GetRatPolyMesh();
+    forall_items(it, S) {
+        IGeometry* geom = _nb.world->CreateGeometry();
+        geom->SetRenderMode(IGeometry::RenderMode::NODES | IGeometry::RenderMode::EDGES | IGeometry::RenderMode::FACES);
+        leda::nb::RatPolyMesh& mesh = geom->GetRatPolyMesh();
 
-        leda::rat_vector v0 = simplices[it].verts[0].to_vector();
-        leda::rat_vector v1 = simplices[it].verts[1].to_vector();
-        leda::rat_vector v2 = simplices[it].verts[2].to_vector();
-        leda::rat_vector v3 = simplices[it].verts[3].to_vector();
+        leda::rat_vector v0 = S[it].verts[0].to_vector();
+        leda::rat_vector v1 = S[it].verts[1].to_vector();
+        leda::rat_vector v2 = S[it].verts[2].to_vector();
+        leda::rat_vector v3 = S[it].verts[3].to_vector();
 
         leda::rat_vector center = leda::rat_vector::zero(3);
         center += v0;
@@ -66,14 +107,18 @@ void Delaunay3D::Invoke() {
         L.push(v1 - center);
         L.push(v2 - center);
         L.push(v3 - center);
-        leda::CONVEX_HULL(L, simplexMesh);
-        simplexMesh.compute_faces();
+        leda::CONVEX_HULL(L, mesh);
+        mesh.compute_faces();
 
-        center *= 2;
-        simplex->SetPosition(
+        geom->SetPosition(
             center.xcoord().to_float(),
             center.ycoord().to_float(),
             center.zcoord().to_float());
+
+        Simplex simplex;
+        simplex.geom    = geom;
+        simplex.center  = center;
+        _simplices.push_back(simplex);
     }
     std::cout << "DONE" << std::endl;
 
--- src/Nubuck/operators/op_delaunay3d/op_delaunay3d.h
+++ src/Nubuck/operators/op_delaunay3d/op_delaunay3d.h
@@ -4,17 +4,41 @@
 #include <Nubuck\operators\operator.h>
 #include <Nubuck\operators\operator_invoker.h>
 
+#include "ui_op_delaunay3d.h"
+
+BEGIN_EVENT_DEF(OP_Delaunay3D_SetScale)
+    int value;
+    int max;
+END_EVENT_DEF
+
 namespace OP {
 
 class Delaunay3DPanel : public OperatorPanel {
+    Q_OBJECT
+private:
+    Ui::Delaunay3D _ui;
+private slots:
+    void OnScaleChanged(int value);
 public:
-    Delaunay3DPanel(QWidget* parent = NULL) : OperatorPanel(parent) { }
+    Delaunay3DPanel(QWidget* parent = NULL);
+
+    void Invoke() override;
 };
 
 class Delaunay3D : public Operator {
 private:
+    struct Simplex {
+        IGeometry*          geom;
+        leda::rat_vector    center;
+    };
+    std::vector<Simplex> _simplices;
+
     Nubuck  _nb;
+
+    void Event_SetScale(const EV::Event& event);
 public:
+    Delaunay3D();
+
     void Register(const Nubuck& nb, Invoker& invoker) override;
     void Invoke() override;
     void Finish() override { }
