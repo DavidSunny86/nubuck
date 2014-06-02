6fee91005b6685592bd1fcd2b708c6ac88e5aba2
Christian Jaeger <johndoe.foobar@googlemail.com>
2014-06-02 08:59:29 +0200

    block signals of up to 3 objects

--- src/Nubuck/UI/block_signals.h
+++ src/Nubuck/UI/block_signals.h
@@ -7,11 +7,46 @@ namespace UI {
 
 class BlockSignals : private GEN::Uncopyable {
 private:
-    QObject*    _obj;
-    bool        _oldState;
+    QObject*    _obj0;
+    QObject*    _obj1;
+    QObject*    _obj2;
+
+    bool        _oldState0;
+    bool        _oldState1;
+    bool        _oldState2;
 public:
-    explicit BlockSignals(QObject* obj) : _obj(obj), _oldState(obj->blockSignals(true)) { }
-    ~BlockSignals() { _obj->blockSignals(_oldState); }
+    explicit BlockSignals(QObject* obj0)
+        : _obj0(obj0)
+        , _obj1(NULL)
+        , _obj2(NULL)
+        , _oldState0(obj0->blockSignals(true))
+        , _oldState1()
+        , _oldState2()
+    { }
+
+    BlockSignals(QObject* obj0, QObject* obj1)
+        : _obj0(obj0)
+        , _obj1(obj1)
+        , _obj2(NULL)
+        , _oldState0(obj0->blockSignals(true))
+        , _oldState1(obj1->blockSignals(true))
+        , _oldState2()
+    { }
+
+    BlockSignals(QObject* obj0, QObject* obj1, QObject* obj2)
+        : _obj0(obj0)
+        , _obj1(obj1)
+        , _obj2(obj2)
+        , _oldState0(obj0->blockSignals(true))
+        , _oldState1(obj1->blockSignals(true))
+        , _oldState2(obj2->blockSignals(true))
+    { }
+
+    ~BlockSignals() {
+        if(_obj0) _obj0->blockSignals(_oldState0);
+        if(_obj1) _obj1->blockSignals(_oldState1);
+        if(_obj2) _obj2->blockSignals(_oldState2);
+    }
 };
 
 } // namespace UI
