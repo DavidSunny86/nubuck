540c0334f4e5ea8a286d7f77d8a4ad666ffaa1a1
Christian Jaeger <johndoe.foobar@googlemail.com>
2014-06-15 15:19:57 +0200

    improved stitching method

--- src/NubuckOperators/alg_flipclip/phase_stitch.cpp
+++ src/NubuckOperators/alg_flipclip/phase_stitch.cpp
@@ -2,65 +2,6 @@
 #include "phase_simplify.h"
 #include "phase_stitch.h"
 
-static bool EdgeEx(leda::nb::RatPolyMesh& mesh, leda::node v0, leda::node v1) {
-    leda::edge e, e0 = nil;
-    forall_out_edges(e, v0) {
-        if(mesh.target(e) == v1) return true;
-    }
-    return false;
-}
-
-// source(fhe) == source(bhe)
-static void StitchVertices(leda::nb::RatPolyMesh& mesh, leda::edge fhe, leda::edge bhe) {
-    leda::node fv = leda::source(fhe);
-    leda::node bv = leda::source(bhe);
-
-    int x = leda::before;
-    leda::edge e = mesh.new_edge(mesh.cyclic_adj_succ(bhe), fv, x, x);
-    leda::edge r = mesh.new_edge(mesh.cyclic_adj_succ(fhe), bv, x, x);
-    mesh.set_reversal(e, r);
-}
-
-// TODO: optimize me!
-static leda::node merge_vertices(leda::nb::RatPolyMesh& mesh, leda::node v0, leda::node v1) {
-    leda::edge e;
-
-    leda::edge e01 = v0->first_out_edge();
-    while(e01 && e01->terminal(1) != v1)
-        e01 = e01->next_out_edge();
-    assert(e01 && e01->terminal(1) == v1);
-
-    leda::edge e10 = mesh.reversal(e01);
-
-    leda::edge i = mesh.cyclic_adj_pred(e01);
-    leda::edge j = mesh.cyclic_adj_pred(e10);
-
-    leda::list<leda::edge> del;
-    while(j != e10) {
-        leda::node w = leda::target(j);
-        leda::edge r = mesh.reversal(j);
-        if(!EdgeEx(mesh, v0, w)) {
-            leda::edge e = mesh.new_edge(i, w);
-            r->set_term(1, v0);
-            mesh.set_reversal(e, r);
-        } else {
-            del.push(r);
-        }
-        j = mesh.cyclic_adj_pred(j);
-    }
-
-    forall(e, del) mesh.del_edge(e);
-    mesh.del_edge(e01);
-
-    v0->append_adj_list(1, v1, 1);
-    mesh.del_node(v1);
-
-    assert(mesh.is_bidirected());
-    assert(mesh.is_map());
-
-    return v0;
-}
-
 inline bool equal_xy(const leda::d3_rat_point& lhp, const leda::d3_rat_point& rhp) {
     return 0 == leda::d3_rat_point::cmp_x(lhp, rhp) && 0 == leda::d3_rat_point::cmp_y(lhp, rhp);
 }
@@ -96,37 +37,71 @@ Phase_Stitch::StepRet::Enum Phase_Stitch::Step() {
         e = mesh.face_cycle_succ(e);
     } while(_g.hullEdges[Side::FRONT] != e);
 
-    leda::edge fhe = _g.hullEdges[Side::FRONT];
-    while(_g.stitchVerts[Side::FRONT] != leda::source(fhe))
-        fhe = mesh.face_cycle_succ(fhe);
+    leda::edge adv0 = _g.hullEdges[Side::FRONT];
+    while(_g.stitchVerts[Side::FRONT] != leda::target(adv0))
+        adv0 = mesh.face_cycle_pred(adv0); // here dir is arbitrary
 
 
-    leda::edge bhe = _g.hullEdges[Side::BACK];
-    while(_g.stitchVerts[Side::BACK] != leda::source(bhe))
-        bhe = mesh.face_cycle_succ(bhe);
+    leda::edge adv1 = _g.hullEdges[Side::BACK];
+    while(_g.stitchVerts[Side::BACK] != leda::source(adv1))
+        adv1 = mesh.face_cycle_succ(adv1); // here dir is arbitrary
 
-    for(int i = 0; i < numHullVerts; ++i) {
-        leda::node fv = leda::source(fhe);
-        leda::node bv = leda::source(bhe);
+    leda::node v0, v1;
+    do {
+        v0 = leda::target(adv0);
+        v1 = leda::source(adv1);
 
-        assert(equal_xy(mesh.position_of(fv), mesh.position_of(bv)));
+        assert(equal_xy(mesh.position_of(v0), mesh.position_of(v1)));
 
-        leda::edge bn = mesh.face_cycle_pred(bhe);
-        leda::edge fn = mesh.face_cycle_succ(fhe);
+        if(0 != leda::d3_rat_point::cmp_z(mesh.position_of(v0), mesh.position_of(v1))) {
+            // stitch
 
-        StitchVertices(mesh, fhe, bhe);
+            int dir = leda::before;
+            leda::edge e = mesh.new_edge(mesh.reversal(adv0), v1, dir, dir);
+            leda::edge r = mesh.new_edge(mesh.cyclic_adj_succ(adv1), v0, dir, dir);
+            mesh.set_reversal(e, r);
 
-        leda::d3_rat_point fp = mesh.position_of(leda::source(fhe));
-        leda::d3_rat_point bp = mesh.position_of(leda::source(bhe));
+            adv0 = mesh.face_cycle_pred(adv0);
+            adv1 = mesh.face_cycle_succ(adv1);
+        } else {
+            // collapse
 
-        if(0 == leda::d3_rat_point::cmp_z(fp, bp)) {
-            merge_vertices(mesh, fv, bv);
+            leda::edge succ0    = mesh.face_cycle_succ(adv0);
+            leda::edge term1    = mesh.cyclic_adj_succ(adv1);
 
-            bhe = mesh.face_cycle_pred(fhe);
-        } else bhe = bn;
+            leda::edge r, next, it1;
 
-        fhe = fn;
-    }
+            it1  = adv1;
+            next = mesh.cyclic_adj_pred(it1);
+
+            if(leda::source(adv0) != leda::target(it1)) {
+                r = mesh.reversal(it1);
+                mesh.move_edge(it1, succ0, leda::target(it1), leda::behind);
+                mesh.move_edge(r, leda::source(r), v0);
+            }
+            it1 = next;
+
+            while(it1 != term1) {
+                next = mesh.cyclic_adj_pred(it1);
+                r = mesh.reversal(it1);
+                mesh.move_edge(it1, succ0, leda::target(it1), leda::behind);
+                mesh.move_edge(r, leda::source(r), v0);
+                it1 = next;
+            }
+
+            if(leda::target(succ0) != leda::target(it1)) {
+                r = mesh.reversal(it1);
+                mesh.move_edge(it1, succ0, leda::target(it1), leda::behind);
+                mesh.move_edge(r, leda::source(r), v0);
+            }
+
+            adv0 = mesh.face_cycle_pred(adv0);
+            adv1 = mesh.face_cycle_succ(adv1);
+
+            mesh.del_node(v1);
+        } // collapse
+
+    } while(_g.stitchVerts[Side::FRONT] != leda::target(adv0));
 
     mesh.compute_faces();
 
