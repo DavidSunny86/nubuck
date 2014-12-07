c580d2fdccc9aefb21b9c1cac20529217750ea09
Christian Jaeger <johndoe.foobar@googlemail.com>
2014-12-07 13:04:49 +0100

    added export macros

--- incl/Nubuck/nubuck_api.h
+++ incl/Nubuck/nubuck_api.h
@@ -7,3 +7,16 @@
 #endif
 
 #define NUBUCK_OPERATOR extern "C" __declspec(dllexport)
+
+#define NUBUCK_EXPORT_OPERATOR(op)                              \
+    NUBUCK_OPERATOR OP::OperatorPanel* CreateOperator() {       \
+        return new op;                                          \
+    }
+
+#define NUBUCK_EXPORT_OPERATOR_AND_PANEL(op, pl)                \
+    NUBUCK_OPERATOR OP::OperatorPanel* CreateOperatorPanel() {  \
+        return new pl;                                          \
+    }                                                           \
+    NUBUCK_OPERATOR OP::Operator* CreateOperator() {            \
+        return new op;                                          \
+    }
