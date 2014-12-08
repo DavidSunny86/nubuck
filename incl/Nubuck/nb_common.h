024a2320b095c5f060af90d98ba417207ed2f0b5
Christian Jaeger <johndoe.foobar@googlemail.com>
2014-12-08 07:57:33 +0100

    added additional headers

--- incl/Nubuck/nb_common.h
+++ incl/Nubuck/nb_common.h
@@ -8,7 +8,16 @@ by most Nubuck clients
 #include <Nubuck\nubuck.h>
 #include <Nubuck\nubuck_api.h>
 
+#include <Nubuck\math\math.h>
+#include <Nubuck\math\vector2.h>
+#include <Nubuck\math\vector3.h>
+#include <Nubuck\math\matrix3.h>
+#include <Nubuck\math\matrix4.h>
+#include <Nubuck\math\quaternion.h>
+
 #include <Nubuck\polymesh.h>
+
+#include <Nubuck\animation\animation.h>
 
 #include <Nubuck\operators\operator.h>
 #include <Nubuck\operators\operator_invoker.h>
